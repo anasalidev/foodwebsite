38cc444b2eb3b7ae83d3ca24aebc41ef1dcd1318
Anas <anasalibinanwar@gmail.com>
2023-09-06 11:29:34 +0500

    Updates

---
+++ precache-manifest.de978b15bb271b0bb81777cc6f31b7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3fc397d11471bcd868f",
+    "url": "/foodwebsite/static/css/main.6de39921.chunk.css"
+  },
+  {
+    "revision": "a3fc397d11471bcd868f",
+    "url": "/foodwebsite/static/js/main.a3fc397d.chunk.js"
+  },
+  {
+    "revision": "85648db6bd117caa1d98",
+    "url": "/foodwebsite/static/css/1.bb328440.chunk.css"
+  },
+  {
+    "revision": "85648db6bd117caa1d98",
+    "url": "/foodwebsite/static/js/1.85648db6.chunk.js"
+  },
+  {
+    "revision": "999d3af37fe2d7427075",
+    "url": "/foodwebsite/static/js/runtime~main.999d3af3.js"
+  },
+  {
+    "revision": "76df48c1c80f474f703097f55fe7f309",
+    "url": "/foodwebsite/index.html"
+  }
+];
